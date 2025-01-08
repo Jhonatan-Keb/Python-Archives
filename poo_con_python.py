c2b0ed185811884efcde0f1b4f1a7f9d1b1ae020
Jhonatan-Keb <jhonykeb123@gmail.com>
2025-01-08 11:27:37 -0600

    Creando atributos de la clase

---
+++ poo_con_python.py
@@ -0,0 +1,23 @@
+class Personaje:
+    #Atributos de la clase 
+    nombre = 'Default'
+    fuerza =  0
+    inteligencia = 0
+    defensa = 0
+    vida = 0
+    #Indicar que no se haga nada en este momento.
+    pass
+#Variable del constructor vacío de la clase
+mi_personaje = Personaje()
+mi_personaje.nombre = "Jhonatan"
+mi_personaje.fuerza = 30
+mi_personaje.inteligencia = 12
+mi_personaje.defensa = 28
+mi_personaje.vida = 3
+print("El nombre del personaje es ", mi_personaje.nombre)
+print("El nombre del personaje es ", mi_personaje.fuerza)
+print("El nombre del personaje es ",mi_personaje.inteligencia)
+print("El nombre del personaje es ",mi_personaje.defensa)
+print("El nombre del personaje es ",mi_personaje.vida)
+
+
